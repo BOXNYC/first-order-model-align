fd4309b02b575218a7fce6259f86d40b28ad61a8
memorz <chenyanbo@bipt.edu.cn>
2022-02-21 19:29:56 +0800

    Update align_images.py
    fix get_file

--- align_images.py
+++ align_images.py
@@ -2,7 +2,7 @@ import os
 import sys
 import bz2
 import argparse
-from keras.utils import get_file
+from tensorflow.keras.utils import get_file
 from face_alignment import image_align
 from landmarks_detector import LandmarksDetector
 import multiprocessing
